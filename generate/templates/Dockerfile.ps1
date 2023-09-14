4c09850b8ef995ded1c846d55173d245fb575b49
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-14 06:35:00 +0000

    Enhancement: Install `easyrsa` manually and change base image to `alpine:3.17`
    This avoids coupling the app with the distro, and also ensures the distro is not stale.

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -1,16 +1,42 @@
 @"
 FROM $( $VARIANT['_metadata']['distro'] ):$( $VARIANT['_metadata']['distro_version'] )
+ARG TARGETPLATFORM
+ARG BUILDPLATFORM
+RUN echo "I am running on `$BUILDPLATFORM, building for `$TARGETPLATFORM"
 
-RUN apk add --no-cache $( $VARIANT['_metadata']['package'] )=$( $VARIANT['_metadata']['package_version'] ) iptables
+RUN apk add --no-cache ca-certificates
 
-COPY docker-entrypoint.sh /docker-entrypoint.sh
-RUN chmod +x /docker-entrypoint.sh
+# Install easyrsa dependencies
+RUN apk add --no-cache iptables openssl
 
-# alpine easyrsa top-level directory. Use command find / -name 'easyrsa'
-RUN echo "Looking for easyrsa binary" \
-    && ls '/usr/share/easy-rsa/easyrsa'
+# Install easyrsa
+# See: https://github.com/OpenVPN/easy-rsa/tree/master/release-keys
+RUN set -eux; \
+    apk add --no-cache gnupg gpg-agent dirmngr; \
+    URL=https://github.com/OpenVPN/easy-rsa/releases/download/$(
+        if ([version]$VARIANT['_metadata']['package_version'] -le [version]'3.0.1') { $VARIANT['_metadata']['package_version'] }
+        else { "v$( $VARIANT['_metadata']['package_version'] )" }
+    )/$(
+        # The prefix for the .tgz file is inconsistent
+        if ([version]$VARIANT['_metadata']['package_version'] -ge [version]'3.0.7') { 'EasyRSA-' }
+        if ([version]$VARIANT['_metadata']['package_version'] -eq [version]'3.0.6') { 'EasyRSA-unix-v' }
+        if ([version]$VARIANT['_metadata']['package_version'] -eq [version]'3.0.5') { 'EasyRSA-nix-' }
+        if ([version]$VARIANT['_metadata']['package_version'] -le [version]'3.0.4') { 'EasyRSA-' }
+    )$( $VARIANT['_metadata']['package_version'] ).tgz; \
+    FILE=`$( basename `$URL ); \
+    wget -q "`$URL"; \
+    wget -q "`$URL.sig"; \
+    gpg --keyserver keys.openpgp.org --recv-keys 6F4056821152F03B6B24F2FCF8489F839D7367F3; \
+    gpg --verify "`$FILE.sig" "`$FILE"; \
+    mkdir -p /usr/share/easy-rsa; \
+    tar -zxvf "`$FILE" --strip-components=1 -C /usr/share/easy-rsa; \
+    /usr/share/easy-rsa/easyrsa help; \
+    rm -fv "`$FILE"; \
+    rm -fv "`$FILE.sig"; \
+    rm -rf /root/.gnupg; \
+    apk del gnupg gpg-agent dirmngr;
+
 ENV EASYRSA=/usr/share/easy-rsa
-
 WORKDIR /usr/share/easy-rsa
 
 # alpine openssl.cnf location. Use command find / -name 'openssl*.cnf'
@@ -18,6 +44,9 @@ WORKDIR /usr/share/easy-rsa
 # >= v3.0.4:
 RUN echo "Looking for openssl.cnf" \
     && find /etc /usr -name 'openssl*.cnf'
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod +x /docker-entrypoint.sh
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
